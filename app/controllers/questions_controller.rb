04910be436d2ee942921f9be0a003a71becde6bc
Leonie Mironesco <leonie.mironesco@hec.edu>
2019-02-21 14:26:42 +0100

    pushed

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,15 @@
 class QuestionsController < ApplicationController
-  # def ask
-  # end
+  def ask
+  end
+
   def answer
-    @questions = ['I am going to work', 'How are you ?', "I don't know what to say"]
+    @user_input = params[:user_input]
+    if @user_input.chars.include?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    elsif @user_input == 'I am going to work'
+      @answer = 'Great'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
 end
